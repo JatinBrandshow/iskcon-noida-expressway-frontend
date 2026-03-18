c5aa5dcf4e06e218e324ad984cfd48fc4cba9597
keshavbrandshow <keshav.saini@brandshow.in>
2026-03-18 11:13:21 +0530

    header

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
 import "@/styles/globals.css";
+import MainLayout from "@/components/MainLayout";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const isAdmin = router.pathname.startsWith("/admin");
+
+  if (isAdmin) {
+    return <Component {...pageProps} />;
+  }
+
+  return (
+    <MainLayout>
+      <Component {...pageProps} />
+    </MainLayout>
+  );
 }
